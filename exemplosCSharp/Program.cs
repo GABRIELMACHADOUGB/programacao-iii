66ac36bea8ad48bb76268a3bc8d4075eb04f5bb3
GABRIEL MACHADO <2025101728@academicougb.com.br>
2026-08-18 19:49:59 -0300

    metros milimetros

--- exemplosCSharp/Program.cs
+++ exemplosCSharp/Program.cs
@@ -1,17 +1,30 @@
-﻿using exemplosCSharp;
+﻿//using exemplosCSharp;
 
-Console.WriteLine("Informe sua idade:");
-int idade = int.Parse(Console.ReadLine());
-Console.WriteLine($"idade: {idade}");
+//Console.WriteLine("Informe sua idade:");
+//int idade = int.Parse(Console.ReadLine());
+//Console.WriteLine($"idade: {idade}");
 
-Console.WriteLine("Informe seu nome:");
-string nome = Console.ReadLine();
-Console.WriteLine($"O nome é: {nome}");
+//Console.WriteLine("Informe seu nome:");
+//string nome = Console.ReadLine();
+//Console.WriteLine($"O nome é: {nome}");
 
-Calculadora calc;
-calc = new Calculadora();
-int resultado = calc.Somar(5, 3);
-Console.WriteLine(resultado);
+//Calculadora calc;
+//calc = new Calculadora();
+//int resultado = calc.Somar(5, 3);
+//Console.WriteLine(resultado);
 
-resultado = Calculadora.Subtrair(10, 4);
-Console.WriteLine(resultado);
+//resultado = Calculadora.Subtrair(10, 4);
+//Console.WriteLine(resultado);
+
+
+// AULA 2
+
+using exemplosCSharp.fixacao;
+
+Console.WriteLine("Informe a medida em metros: ");
+
+int metros = int.Parse(Console.ReadLine());
+
+int milimetros = MetrosMilimetros.Converter(metros);
+
+Console.WriteLine($"Conversão: {metros}m -> {milimetros}mm");
---
+++ exemplosCSharp/fixacao/MetrosMilimetros.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exemplosCSharp.fixacao
+{
+    internal class MetrosMilimetros
+    {
+        public static int Converter(int metros)
+        {
+            return metros * 1000;
+        }
+
+
+    }
+}
